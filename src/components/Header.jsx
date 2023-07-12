60101036a02e6d58405c061b823be8ccf3da71eb
kbsaeeddev <saeed.dev+edi@fascom.com>
2023-07-12 19:03:45 +0500

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,31 @@
-import React from 'react'
+import React from "react";
 import logo from "../assets/Logo.png";
-import {BiCalendar} from "react-icons/bi";
-
-
+import { BiCalendar } from "react-icons/bi";
 
 const Header = () => {
   return (
     <>
-    <nav className="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-    <div className="navbar-brand-wrapper d-flex justify-content-center align-items-center">
-      <div className="navbar-brand-inner-wrapper d-flex  align-items-center justify-content-center w-100">
-        <div className="navbar-brand brand-logo">
-          <img src={logo} alt="" />
-        </div>
-
-
-      </div>
-    </div>
-    <div className="navbar-menu-wrapper d-flex align-items-center justify-content-end">
-      <ul className="navbar-nav navbar-nav-right">
-        <li className="nav-item nav-date dropdown">
-          <div
-            
-            className="nav-link d-flex justify-content-center align-items-center"
-          >
-            <h6 className="date mb-0 mt-0 mr-8">Today : Mar 23</h6>
-            <BiCalendar className="date" />
+      <nav className="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+        <div className="navbar-brand-wrapper d-flex justify-content-center align-items-center">
+          <div className="navbar-brand-inner-wrapper d-flex  align-items-center justify-content-center w-100">
+            <div className="navbar-brand brand-logo">
+              <img src={logo} alt="" />
+            </div>
           </div>
-        </li>
-      </ul>
-    </div>
-  </nav>  
+        </div>
+        <div className="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+          <ul className="navbar-nav navbar-nav-right">
+            <li className="nav-item nav-date dropdown">
+              <div className="nav-link d-flex justify-content-center align-items-center">
+                <h6 className="date mb-0 mt-0 mr-8">Today : Mar 23</h6>
+                <BiCalendar className="date" />
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-  
-  
-  </>)
-}
-
-export default Header
+export default Header;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NotFound from "../screens/NotFound/NotFound"
-import Dashboard from "../screens/Dashboard/Dashboard"
-
+import NotFound from "../screens/NotFound/NotFound";
+import Dashboard from "../screens/Dashboard/Dashboard";
 
 export default function AllRoutes() {
-	return (
-        <Router>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </Router>
-	);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
