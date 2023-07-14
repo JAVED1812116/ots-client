c9e8beee75ba71ec70d0260a3f41abbe788a3569
JAVED1812116 <73031861+JAVED1812116@users.noreply.github.com>
2023-07-14 14:31:19 +0500

    work done

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,6 +5,8 @@ import Dashboard from "../screens/Dashboard/Dashboard";
 import LandlordSignUp from "../screens/Landlord/Auth/LandlordSignUp";
 import LandlordLogin from "../screens/Landlord/Auth/LandlordLogin";
 import LandLordDashboard from "../screens/Landlord/Dashboard/Dashboard";
+import NewRequest from "../screens/Landlord/NewRequest/NewRequest";
+import RequestUserDetail from "../screens/Landlord/RequestUserDetail/RequestUserDetail";
 export default function AllRoutes() {
   return (
     <Router>
@@ -13,6 +15,8 @@ export default function AllRoutes() {
         <Route path="/landlord-signup" element={<LandlordSignUp />} />
         <Route path="/landlord-login" element={<LandlordLogin />} />
         <Route path="/landlord-dashboard" element={<LandLordDashboard />} />
+        <Route path="/new-request" element={<NewRequest />} />
+        <Route path="/user-detail" element={<RequestUserDetail />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </Router>
