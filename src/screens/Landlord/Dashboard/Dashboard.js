93a5d3702f2808becb144405a0e1f6d91697d802
JAVED1812116 <73031861+JAVED1812116@users.noreply.github.com>
2023-07-18 18:27:55 +0500

    dashbord resady

--- src/screens/Landlord/Dashboard/Dashboard.js
+++ src/screens/Landlord/Dashboard/Dashboard.js
@@ -1,42 +1,276 @@
-import React from 'react'
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import GroupsRoundedIcon from '@mui/icons-material/GroupsRounded';
-import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
-import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlined';
-import { Button } from '@mui/material';
-import "./dashboard.css"
-const Dashboard = () => {
+import { useState } from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import AddIcon from '@mui/icons-material/Add';
+import FolderSharedIcon from '@mui/icons-material/FolderShared';
+import NewRequest from '../NewRequest/NewRequest';
+import Logo from "../../../assets/Logo.png"
+import { AiFillHome,AiFillSetting } from 'react-icons/ai';
+import { IoMdAdd ,IoIosPeople} from 'react-icons/io';
+import { MdAccountBalance} from 'react-icons/md';
+
+// import { FaPeopleGroup } from 'react-icons/fa';
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
+export default function Dashboard() {
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+  const [menuData, setMenuData] = useState("Home");
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div style={{flex:1}}>
-    
-     <div className='addNewTenant'>
-<PersonAddIcon style={{width:111,height:200,alignItems:'center',justifyContent:'center',marginLeft:85}}/>
-<text style={{marginLeft:-130,fontSize:22,fontWeight:'bold'}}>Add New Tenant</text>
-     </div>
+    <>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar style={{backgroundColor:'black'}}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+                 marginRight: 5,
+                 ...(open && { display: 'none' }),
+               }}
+          >
+         
+            <MenuIcon />
+            
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+           OTS
+          </Typography>
+               {/* <Box
+                   component="img"
+                   sx={{
+                   height: 74,
+                   marginLeft:180
+                   }}
+                   alt="Your logo."
+                   src={Logo}
+               /> */}
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+     
+            <ListItem disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                    
+                   <AiFillHome />
+                </ListItemIcon>
+                <ListItemText primary={open ? "Home" : null}  />
+              </ListItemButton>
+            </ListItem>
 
-     <div className='newRequest'>
-<AddCircleIcon style={{width:111,height:200,alignItems:'center',justifyContent:'center',marginLeft:85}}/>
-<text style={{marginLeft:-130,fontSize:22,fontWeight:'bold'}}>New Request</text>
-     </div>
+            <ListItem disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                    
+                   <IoMdAdd />
+                </ListItemIcon>
+                <ListItemText primary={open ? "Add New Tenant" : null}  />
+              </ListItemButton>
+            </ListItem>
 
-     <div className='allTenant'>
-<GroupsRoundedIcon style={{width:111,height:200,alignItems:'center',justifyContent:'center',marginLeft:100}}/>
-<text style={{marginLeft:-110,fontSize:22,fontWeight:'bold'}}>All Tenant</text>
-     </div>
+            <ListItem disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                    
+                   <IoIosPeople />
+                </ListItemIcon>
+                <ListItemText primary={open ? "All Tenant" : null}  />
+              </ListItemButton>
+            </ListItem>
 
-     <div className='rentSetting'>
-<SettingsRoundedIcon style={{width:111,height:200,alignItems:'center',justifyContent:'center',marginLeft:100}}/>
-<text style={{marginLeft:-110,fontSize:22,fontWeight:'bold'}}>Rent Setting</text>
-     </div>
+            <ListItem disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                    
+                   <AiFillSetting />
+                </ListItemIcon>
+                <ListItemText primary={open ? "Rent Setting" : null}  />
+              </ListItemButton>
+            </ListItem>
 
-     <div className='accountDetail'>
-<AccountBalanceOutlinedIcon style={{width:111,height:200,alignItems:'center',justifyContent:'center',marginLeft:100}}/>
-<text style={{marginLeft:-110,fontSize:22,fontWeight:'bold'}}>Account Detail</text>
-     </div>
-   
-    </div>
-  )
+            <ListItem disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                    
+                   <MdAccountBalance />
+                </ListItemIcon>
+                <ListItemText primary={open ? "ACCOUNT DETAIL" : null} />
+              </ListItemButton>
+            </ListItem>
+     
+        </List>
+        <Divider />
+       
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        {
+          menuData=='Home'
+        }
+      </Box>
+    </Box>
+    </>
+  );
 }
 
-export default Dashboard
