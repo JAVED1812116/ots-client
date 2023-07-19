ffa94cab92b5da17a76855c21213057e5cfcd67b
kbsaeeddev <saeed.dev+edi@fascom.com>
2023-07-19 18:53:31 +0500

    button component

---
+++ src/components/ButtonComp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+const ButtonComp = () => {
+  return (
+    <Button
+      variant="contained"
+    >
+      Accept
+    </Button>
+  );
+};
+
+export default Button;
