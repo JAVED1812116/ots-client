888d738e8b98210866c5e482e85c24d1f872eeaa
JAVED1812116 <73031861+JAVED1812116@users.noreply.github.com>
2023-08-17 17:56:59 +0500

    toolkit

---
+++ src/store/slices/UserSlice.jsx
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice=createSlice({
+    name:"user",
+    initialState:[],
+    reducers:{
+        addUser(state,action){ },
+        removeUser(state,action){ },
+        deleteUsers(state,action){ }
+    }
+})
+console.log(userSlice.actions,"dekhhhhh")
+export default userSlice.reducer
