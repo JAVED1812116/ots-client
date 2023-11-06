2f1e851b9484a757473af7ad65f6ba2f9cbe2e06
JAVED1812116 <73031861+JAVED1812116@users.noreply.github.com>
2023-11-06 16:44:19 +0500

    email Api integrate

--- src/screens/Landlord/AddNewTenant/AddNew.js
+++ src/screens/Landlord/AddNewTenant/AddNew.js
@@ -13,14 +13,47 @@ import {
 import MarkunreadIcon from "@mui/icons-material/Markunread";
 import SendIcon from "@mui/icons-material/Send";
 import title from "../../../components/title";
-import "./newRequest.css"
-
+import "./newRequest.css";
+import { SendMail } from "../../../Redux/Reducer/SendEmail";
+import { useDispatch } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
 export default function AddNew() {
   title("Add New Tenant");
+  const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
   const location = useLocation();
   const [mylocation, setMyLocation] = useState(location.pathname);
+  const [email, setEmail] = useState(null);
+  const sendEMAIL = () => {
+    if (email != null || "") {
+      var regexEmail = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
 
+      if (regexEmail.test(email)) {
+        dispatch(SendMail({ email })).then((res) => {
+          console.log(res?.payload?.data, "ressssssssss");
+          if (res?.payload?.data === "Email Recieved") {
+            toast.success(res?.payload?.data, {
+              position: "top-center",
+            });
+          } else {
+            toast.error("Something Wrong", {
+              position: "top-center",
+            });
+          }
+
+        });
+      }else{
+        toast.error("Email is not valid", {
+          position: "top-center",
+        });
+      }
+    }
+    else{
+      toast.error("Empty Field are not allowed", {
+        position: "top-center",
+      });
+    }
+  };
   return (
     <>
       <Wrapper open={open} setOpen={setOpen} mylocation={mylocation} />
@@ -29,22 +62,33 @@ export default function AddNew() {
           <h1>Add New Tenant</h1>
         </div>
         <Container maxWidth="sm" className="add-new-container">
-        <FormControl className="w100">
-          {/* <InputLabel htmlFor="input-with-icon-adornment">Email</InputLabel> */}
-          <Input
-            id="input-with-icon-adornment"
-            placeholder={"Enter Email Address"}
-            startAdornment={
-              <InputAdornment position="start">
-                <MarkunreadIcon />
-              </InputAdornment>
-            }
-          />
-        </FormControl>
-        <Button className="addNewButton" variant="contained" endIcon={<SendIcon />}>
-          Send
-        </Button>
+          <FormControl className="w100">
+            {/* <InputLabel htmlFor="input-with-icon-adornment">Email</InputLabel> */}
+            <Input
+              id="input-with-icon-adornment"
+              placeholder={"Enter Email Address"}
+              startAdornment={
+                <InputAdornment position="start">
+                  <MarkunreadIcon />
+                </InputAdornment>
+              }
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </FormControl>
+          <Button
+            className="addNewButton"
+            variant="contained"
+            endIcon={<SendIcon />}
+            onClick={() => {
+              sendEMAIL();
+            }}
+          >
+            Send
+          </Button>
         </Container>
+        <ToastContainer />
       </div>
     </>
   );
