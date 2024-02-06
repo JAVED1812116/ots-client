4463855138eb85bf119d2605c05b5a946465485a
kbsaeeddev <saeed.dev+edi@fascom.com>
2024-02-06 11:09:18 +0500

    update css

--- src/screens/Landlord/NewRequest/NewRequest.js
+++ src/screens/Landlord/NewRequest/NewRequest.js
@@ -250,7 +250,7 @@ React.useEffect(()=>{
           <TableCell align="right">{row.flatAdvance}</TableCell>
         </TableRow>
         <TableRow>
-          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={8}>
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={12}>
             <Collapse in={open} timeout="auto" unmountOnExit>
               <Box sx={{ margin: 1 }}>
                 <Typography variant="h6" gutterBottom component="div">
